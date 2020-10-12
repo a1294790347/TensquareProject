a17a28fec0a9647b1aaf806f9de73fe7d2d93dce
a1294790347 <70732810+a1294790347@users.noreply.github.com>
2020-10-12 14:32:47 +0800

    Update SendSms.java

--- tensquare_sms/src/main/java/com/tensquare/util/SendSms.java
+++ tensquare_sms/src/main/java/com/tensquare/util/SendSms.java
@@ -11,13 +11,13 @@ import com.aliyuncs.profile.DefaultProfile;
 
 public class SendSms {
     //对应你阿里云账户的accessKeyId
-    private static final String accessKeyId="LTAI4G8qSU2W8MpPmgTC2ZxP";
+    private static final String accessKeyId="";
     //对应你阿里云账户的accessKeySecret
-    private static final String accessKeySecret="DEU3BQbLLruopLp8bj6hs12DgcJJ8l";
+    private static final String accessKeySecret="";
     //对应你阿里云的签名名称
     private static final String signName="十次方项目练习";
     //对应你阿里云的模板code
-    private static final String templateCode="SMS_202730066";
+    private static final String templateCode="";
 
     public static void SendCode(String telphone, String code) {
         //参数服务器名称
